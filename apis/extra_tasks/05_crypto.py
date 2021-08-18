1d9a11dc1a241fa8af50992704b2117c0951d16b
csulva <chrissulva1@gmail.com>
2021-08-18 17:24:14 -0400

    new API labs

--- apis/extra_tasks/05_crypto.py
+++ apis/extra_tasks/05_crypto.py
@@ -14,3 +14,29 @@ HINTS:
 BONUS: Explore the logging package for easier tracking
 
 '''
+
+import requests
+import datetime
+import time
+
+#url = "https://api.nomics.com/v1/currencies?key=a2388e583fb00867472c69b07bb5a1fb9dc2966c&ids=BTC,ETH,XRP&attributes=id,name,logo_url"
+url = "https://api.nomics.com/v1/currencies/ticker?key=a2388e583fb00867472c69b07bb5a1fb9dc2966c&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1"
+response = requests.get(url)
+response = response.json()
+
+response = response[0]
+
+saved_BTC_date = {}
+ten_min = time.time() + 60 + 10
+
+while time.time() < ten_min:
+    #every ten seconds
+    time.sleep(10)
+    for key, value in response.items():
+        if key == 'price':
+            saved_BTC_date[str(datetime.datetime.now())] = value
+            append = open('/Users/christophersulva/Desktop/API_project/API_Labs/python_apis_databases/apis/extra_tasks/Bitcoin_data.csv', 'w')
+            append.write(f'{saved_BTC_date}')
+            append.close()
+
+
