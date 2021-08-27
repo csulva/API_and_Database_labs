e5b922d43e99d5f1eb054d80511c37ffb0ea9c9f
csulva <chrissulva1@gmail.com>
2021-08-26 22:35:09 -0400

    first database lab commit

--- databases/Exercise_01.py
+++ databases/Exercise_01.py
@@ -5,3 +5,26 @@ All of the following exercises should be done using sqlalchemy.
 Using the provided database schema, write the necessary code to print information about the film and category table.
 
 '''
+import sqlalchemy
+from pprint import pprint
+
+engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/sakila')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+film = sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)
+category = sqlalchemy.Table('category', metadata, autoload=True, autoload_with=engine)
+
+print('film:')
+print(film.columns.keys())
+print(repr(metadata.tables['film']))
+print('category:')
+print(category.columns.keys())
+print(repr(metadata.tables['category']))
+
+#query = sqlalchemy.select([film])
+
+# result_proxy = connection.execute(query)
+# result_set = result_proxy.fetchall()
+
+# pprint(result_set)
--- databases/Exercise_02.py
+++ databases/Exercise_02.py
@@ -1,16 +1,37 @@
-'''
-Consider each of the tasks below as a separate database query. Using SQLAlchemy, which is the necessary code to:
+#Consider each of the tasks below as a separate database query. Using SQLAlchemy, which is the necessary code to:
 
-- Select all the actors with the first name of your choice
+import re
+import sqlalchemy
+from pprint import pprint
 
-- Select all the actors and the films they have been in
+from sqlalchemy.sql.expression import join
 
-- Select all the actors that have appeared in a category of a comedy of your choice
+engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/sakila')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
 
-- Select all the comedic films and sort them by rental rate
+#Select all the actors with the first name of your choice
+actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+# query = sqlalchemy.select([actor]).where(actor.columns.first_name=='PENELOPE')
+# result_proxy = connection.execute(query)
+# result = result_proxy.fetchall()
+# pprint(result)
 
-- Using one of the statements above, add a GROUP BY statement of your choice
+#Select all the actors and the films they have been in
+film = sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)
+film_actor = sqlalchemy.Table('film_actor', metadata, autoload=True, autoload_with=engine)
 
-- Using one of the statements above, add a ORDER BY statement of your choice
+join_statement = actor.join(film_actor, actor.columns.actor_id == film_actor.columns.actor_id).join(film, film.columns.film_id == film_actor.columns.film_id)
+# query = sqlalchemy.select([actor.columns.actor_id, actor.columns.first_name, actor.columns.last_name, film.columns.title]).select_from(join_statement)
+# result_proxy = connection.execute(query)
+# result = result_proxy.fetchall()
+# pprint(result)
 
-'''
+#Select all the actors that have appeared in a category of a comedy of your choice
+
+#Select all the comedic films and sort them by rental rate
+
+#Using one of the statements above, add a GROUP BY statement of your choice
+
+#Using one of the statements above, add a ORDER BY statement of your choice
+
