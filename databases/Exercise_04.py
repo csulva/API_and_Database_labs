91ee298bf5b8c2d09e0f2f3f605f9b271b1dd9ec
csulva <chrissulva1@gmail.com>
2021-08-29 19:55:56 -0400

    New edits to database exercise application

--- databases/Exercise_04.py
+++ databases/Exercise_04.py
@@ -17,7 +17,7 @@ import sqlalchemy
 from pprint import pprint
 import time
 
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import text
 
 engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/SocialDB')
 connection = engine.connect()
@@ -39,12 +39,15 @@ user_input = input('''Please select your task:
 2. Create a new table
 3. Add data to a table
 4. Update data in a table
-5. Delete data from a table
-6. Delete a table
-7. Compare data between two tables
+5. Add new column to table
+6. Delete data from a table
+7. Delete a table
+8. Compare data between two tables
 ''')
 
+
 # Functions
+# 1
 def show_data():
     new_input = input(f'For which data would you like to view the data?\n1. {list_of_tables[0]}\n2. {list_of_tables[1]}\n3. {list_of_tables[2]}\n4. {list_of_tables[3]}\n')
     while new_input == '1' or new_input == '2' or new_input == '3' or new_input == '4':
@@ -75,10 +78,12 @@ def show_data():
         else:
             new_input == new_input
 
+# 2
 def create_table():
     global metadata
     name_table = input('Please name your table: ')
     print(f'Your new table is called {name_table}. It will have a primary key column titled {name_table}_id.')
+    time.sleep(1)
     new_column = input(f'Would you like to create a new column for {name_table} table? Type "yes" to create a new column. Type "no" or "quit" to stop. ')
     if new_column == 'quit'.lower() or new_column == 'no'.lower():
         new_table = sqlalchemy.Table(f'{name_table}', metadata,
@@ -99,39 +104,73 @@ def create_table():
         else:
             data_type = input('What is the datatype of this column? Type "int" for integer, "float" for a floating number, "string" for string, or "bool" for a boolean value. ')
             if data_type == 'int':
-                new_table = sqlalchemy.Table(f'{name_table}', metadata,
-                sqlalchemy.Column(f'{name_table}_id', sqlalchemy.Integer()),
-                sqlalchemy.Column(column_1, sqlalchemy.Integer(), nullable=False))
-                metadata.create_all(engine)
-                list_of_tables.append(name_table)
-                print(f'You created a {name_table} table. To add data to it, please restart the application and select option 3. ')
-                quit()
+                data_type = sqlalchemy.Integer()
             elif data_type == 'float':
-                new_table = sqlalchemy.Table(f'{name_table}', metadata,
-                sqlalchemy.Column(f'{name_table}_id', sqlalchemy.Integer()),
-                sqlalchemy.Column(column_1, sqlalchemy.Float(), nullable=False))
-                metadata.create_all(engine)
-                list_of_tables.append(name_table)
-                print(f'You created a {name_table} table. To add data to it, please restart the application and select option 3. ')
-                quit()
+                data_type = sqlalchemy.Float()
             elif data_type == 'string':
                 length = input('How long would you like your string to be, maximum? Type a number: ')
-                length = int(length)
-                new_table = sqlalchemy.Table(f'{name_table}', metadata,
-                sqlalchemy.Column(f'{name_table}_id', sqlalchemy.Integer()),
-                sqlalchemy.Column(column_1, sqlalchemy.String(length), nullable=False))
-                metadata.create_all(engine)
-                list_of_tables.append(name_table)
-                print(f'You created a {name_table} table. To add data to it, please restart the application and select option 3. ')
-                quit()
+                data_type = sqlalchemy.String(int(length))
             elif data_type == 'bool':
-                new_table = sqlalchemy.Table(f'{name_table}', metadata,
+                data_type = sqlalchemy.Boolean()
+            new_table = sqlalchemy.Table(f'{name_table}', metadata,
                 sqlalchemy.Column(f'{name_table}_id', sqlalchemy.Integer()),
-                sqlalchemy.Column(column_1, sqlalchemy.Boolean(), nullable=False))
-                metadata.create_all(engine)
-                list_of_tables.append(name_table)
-                print(f'You created a {name_table} table. To add data to it, please restart the application and select option 3. ')
-                quit()
+                sqlalchemy.Column(column_1, data_type, nullable=False))
+            print(f'Executing new table {name_table}...')
+            metadata.create_all(engine)
+            list_of_tables.append(new_table)
+            time.sleep(2)
+            print(f'You successfully created a {name_table} table. To add data to it, please restart the application and select option 3. ')
+            quit()
+
+# 3
+def add_data_to_table():
+    print('Tables: ')
+    for tables in list_of_tables:
+        print(tables)
+    select_table = input('Which table would you like to add data to? ')
+
+    # pprint(repr(metadata.tables[select_table].columns.keys()))
+    if select_table == 'Users':
+        first_name = input("What's the first name? ")
+        last_name = input("What's the last name? ")
+        email = input("What's the email address? ")
+        query = sqlalchemy.insert(users).values(UserId = 100, FirstName=first_name, LastName=last_name, Email=email)
+        result_proxy = connection.execute(query)
+        print('Success!')
+    elif select_table == 'Images':
+        url = input("What's the url of the image you would like to post? ")
+        query = sqlalchemy.insert(images).values(ImageId = 100, ImageURL=url)
+        result_proxy = connection.execute(query)
+        print('Success!')
+    while select_table != 'Users' or select_table != 'Images' or select_table != 'Posts' or select_table != 'Users_Friends':  
+        print('\nType in the correct table name from the list below.')
+        add_data_to_table()
+    quit()
+
+    
+
+# 5
+def add_column():
+    print('Tables: ')
+    for tables in list_of_tables:
+        print(tables)
+    select_table = input('Which table would you like to add a column to? ')
+    selected_table = sqlalchemy.Table(f'{select_table}', metadata, autoload=True, autoload_with=engine)
+    column_name = input('What would you like to name the column? ')
+    data_type = input('What is the datatype of this column? Type "int" for integer, "float" for a floating number, "string" for string, or "bool" for a boolean value. ')
+    if data_type == 'int':
+        data_type = sqlalchemy.Integer()
+    elif data_type == 'float':
+        data_type = sqlalchemy.Float()
+    elif data_type == 'string':
+        length = input('How long would you like your string to be, maximum? Type a number: ')
+        data_type = sqlalchemy.String(int(length))
+    elif data_type == 'bool':
+        data_type = sqlalchemy.Boolean()
+    new_column = sqlalchemy.Table(selected_table, metadata).update(text(column_name), data_type)
+    results_proxy = connection.execute(new_column)
+    print(f'Creating new column {column_name} in table {selected_table}')
+    metadata.create_all(engine)
 
 
 while user_input:
@@ -139,3 +178,8 @@ while user_input:
         show_data()
     if user_input == '2':
         create_table()
+    if user_input == '3':
+        add_data_to_table()
+
+    if user_input == '5':
+        add_column()
